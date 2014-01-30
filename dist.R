9f0e1913cdc7cafef69d363a43f3cb1233b59788
kylec <kylelyk02@gmail.com>
2014-01-30 17:33:42 -0600

    af vs sample, af vs cov

--- dist.R
+++ dist.R
@@ -1,5 +1,25 @@
+# sum af across samples
 setwd('~/Projects/raf')
+d = read.table('out.txt', header=T)
+head(d)
+dim(d)
+# summary of af dist
+hist(d$refCount/d$total, breaks=seq(0,1,.005))
+summary(d$refCount/d$total)
 
+# #samples vs #sites
+#hist(log2(d$sample), breaks=seq(0,42,1))
+hist(d$sample, breaks=seq(0,42,1))
+summary(d$sample)
+
+# # samples vs #af
+plot(d$sample, d$refCount/d$total)
+
+# # cov vs #af
+plot(d$total, d$refCount/d$total, xlim=c(0,10000))
+
+##------------------------------------------------##
+# af distribution of each samples
 files = system('ls TCGA*.txt | head -10', intern=T)
 names = system('ls TCGA*.txt | cut -d- -f2,3 | sed \'s/.txt//\'| head -10', intern=T)
 # matrix of samples af
@@ -8,7 +28,6 @@ d_tr=c()
 d_tv=c()
 stats = c()
 
-# read af 
 for (i in 1:length(files)) {
   a = read.table(files[i], header=TRUE, sep='\t')
   tr = a[which( ((a$ref=='G' & a$var=='A') | (a$ref=='A' & a$var=='G') | (a$ref=='C' & a$var=='T') | (a$ref=='T' & a$var=='C')) ), ]
